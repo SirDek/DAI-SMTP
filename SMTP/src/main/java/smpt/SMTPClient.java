b21d4bad9cf0dd64aa2a9d1234db6223a0efd2db
cedric <cedric.centeno@gmail.com>
2022-11-24 13:27:34 +0100

    email avec entete, reste subject

--- SMTP/src/main/java/smpt/SMTPClient.java
+++ SMTP/src/main/java/smpt/SMTPClient.java
@@ -18,27 +18,23 @@ public class SMTPClient {
 
     public void send() {
 
-        final String SMTPADDR = server.getHost();//mail.getaddr
+        final String SMTP_HOST = server.getHost();//mail.getaddr
         final int port = server.getPort(); //mail.getport
 
-        final String SEND = ".\r\n";
+        final String END_LINE = "\r\n";
         final String FORMAT = "UTF-8";
         final String ERROR_MSG = "ERROR with message recieved : ";
 
         Socket socket = null;
         BufferedReader in = null;
         BufferedWriter out = null;
-
         try {
-            socket = new Socket(SMTPADDR, port);
-
+            socket = new Socket(SMTP_HOST, port);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream(), FORMAT));
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), FORMAT));
             String line = in.readLine();
-
-            out.write("EHLO mock");
+            out.write("EHLO mock" + END_LINE);
             out.flush();
-
             line = in.readLine();
             if (!line.startsWith("250")) {
                 throw new IOException(ERROR_MSG + line);
@@ -48,16 +44,15 @@ public class SMTPClient {
                 line = in.readLine();
             }
 
-            out.write("MAIL FROM:" + mail.getSender());// + adresse d'envoi);
+            out.write("MAIL FROM:" + mail.getSender() + END_LINE);// + adresse d'envoi);
             out.flush();
-
             line = in.readLine();
             if (!line.startsWith("250")) {
                 throw new IOException(ERROR_MSG + line);
             }
 
             for (String reciever : mail.getReceivers()) {
-                out.write("RCPT TO:" + reciever);// ;
+                out.write("RCPT TO:" + reciever + END_LINE);// ;
                 out.flush();
                 line = in.readLine();
                 if (!line.startsWith("250")) {
@@ -65,18 +60,35 @@ public class SMTPClient {
                 }
             }
 
-            out.write("DATA");
+            out.write("DATA" + END_LINE);
             out.flush();
-            if (line.startsWith("4") || line.startsWith("5")) {
+            line = in.readLine();
+            if (line.startsWith("4") || line.startsWith("5")) { // todo faire mieux
                 throw new IOException(ERROR_MSG + line);
             }
+            // entêtes email
+            out.write("From: " + mail.getSender() + END_LINE);
+            out.write("To: ");
+            for (String reciever : mail.getReceivers()) {
+                out.write( reciever + " ");
+            }
 
+            //out.write("From: " + mail.getSubject() + END_LINE);
+
+            out.write(END_LINE + END_LINE);
             out.write(mail.getFakeMail());
-            out.write(SEND);
+            out.write('.' +END_LINE);
+            out.flush();
+
             line = in.readLine();
             if (!line.startsWith("250")) {
                 throw new IOException(ERROR_MSG + line);
             }
+
+            in.close();
+            out.close();
+            socket.close();
+
         } catch (IOException ex) {
             if (in != null) {
                 try {
